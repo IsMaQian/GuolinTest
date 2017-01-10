1cde55ff2e5ce69ba78dac30837453c3cdd9bdcb
IsMaQian <252919181@qq.com>
2017-01-10 20:23:48 +0800

    toolbar success

--- toolbar/src/main/java/com/example/mq/toolbar/FruitAdapter.java
+++ toolbar/src/main/java/com/example/mq/toolbar/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mq.toolbar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -45,8 +47,20 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
             mContext = parent.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, parent, false);
-
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+//                Toast.makeText(mContext,"clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        return holder;
     }
 
     @Override
